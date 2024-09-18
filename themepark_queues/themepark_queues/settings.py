fa1dcdded9ed0fd66820c30868d9e66b3f14eb58
Ryan Gascoigne-Jones <rtg204@exeter.ac.uk>
2024-09-18 18:29:31 +0100

    deployment settings

--- themepark_queues/themepark_queues/settings.py
+++ themepark_queues/themepark_queues/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from django.core.management.utils import get_random_secret_key
 
 # TODO: Not neccessary when deployed
 # Loads in environment variables
@@ -26,13 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&0qn%o4c_#+4ky)#eqs*=1y563kus2$2ac9&to^%as^$uj@p)_"
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-  os.getenv('LOCAL_IP'),
+  os.getenv('PUBLIC_IP'),
   'localhost'
 ]
 
